cdd235cc1b0cab98720bdd9624ee4140eaae17d1
Gianella Leon <giigl@hotmail.com>
2022-11-19 19:16:53 -0500

    Navbar sin searchBar

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,6 +1,8 @@
+import { NavBar } from "../../components/Nav/Nav"
 export const Home=()=>{
     return(
         <div>
+            <NavBar/>
             Home.
         </div>
     )
--- src/view/PerfilProfesor/PerfilProfesor.jsx
+++ src/view/PerfilProfesor/PerfilProfesor.jsx
@@ -1,6 +1,8 @@
-export const PerfilAlumno=()=>{
+import { NavBar } from "../../components/Nav/Nav"
+export const PerfilProfesor=()=>{
     return(
         <div>
+            <NavBar/>
             Perfil Profesor
         </div>
     )
