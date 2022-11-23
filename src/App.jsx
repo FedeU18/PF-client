887c406a79609cbb3d5920e3075133d013a9b360
Michael Canales <CDLSMichael.16@gmail.com>
2022-11-23 16:28:11 -0500

    autenticacion y redirection a home

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { Routes, Route } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useContext, useEffect, useState } from 'react';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { AuthContext } from './Authentication/context/AuthContext';
+import { auth } from './Authentication/firebase/credenciales';
+import ProtectedRoute from './Authentication/protection/ProtectedRoute';
 import { Home } from './view/Home/Home'
 import Landing from './view/Landing/Landing';
 import { Perfil } from './view/Perfil/Perfil'
 
 function App() {
+  const navigate = useNavigate()
+  const user = useContext(AuthContext)
+
+  useEffect(() => {
+
+    if(user){
+      navigate("/home")
+    }else {
+      navigate("/")
+    }
+  }, [user])
+  
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} />
-        <Route path="/profile" element={<Perfil />} />
+        {/* <Route element={<ProtectedRoute user={user} />}> */}
+          <Route path='/' element={<Landing />} />
+          <Route path='/home' element={<Home />} />
+          <Route path="/profile" element={<Perfil />} />
+        {/* </Route> */}
       </Routes>
     </div>
   )
--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,10 +1,42 @@
+import logOut from "../../Authentication/functions/logOut"
 import { NavBar } from "../../components/Nav/Nav"
+import { useNavigate } from "react-router-dom"
+import { useContext } from "react"
+import { AuthContext } from "../../Authentication/context/AuthContext"
 
-export const Home=()=>{
-    return(
+export const Home = () => {
+    const user = useContext(AuthContext)
+    console.log(user)
+    const navigate = useNavigate()
+
+    const CloseMySesion = () => {
+        logOut()
+        navigate("/")
+    }
+
+    return (
         <div>
-            <NavBar/>
+            <NavBar />
             Home.
+            <button
+                className="btn btn-danger btn-sm"
+                onClick={CloseMySesion}>
+                Log out
+            </button>
+            <br />
+
+            <h1>Hola :)!!!</h1>
+            <h2>
+                {user ?
+                    user.currentUserData.name ||
+                    user.displayName
+                    : null}
+            </h2>
+            <h2>
+                email : {user ? user.email || user.currentUserData.email : null}
+            </h2>
+
+            <img src="" alt={user ? user.displayName : null} />
         </div>
     )
 }
