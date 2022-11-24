a142679b5fdb79d724bcd4198ef953de500cdcf1
Gianella Leon <giigl@hotmail.com>
2022-11-24 17:45:38 -0500

    about

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import { Home } from './view/Home/Home'
 import { Landing } from './view/Landing/Landing';
 import { Perfil } from './view/Perfil/Perfil'
 import {Error} from "./view/Error/error";
+import {About} from "./view/About/About"
 
 function App() {
   const user = useContext(AuthContext)
   const navigate = useNavigate()
 
-  useEffect(() => {
-    if (user) {
-      navigate("/home")
-    } else {
-      navigate("/")
-    }
-  }, [user])
+  // useEffect(() => {
+  //   if (user) {
+  //     navigate("/home")
+  //   } else {
+  //     navigate("/")
+  //   }
+  // }, [user])
 
 
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path='/' element={<Landing/>}/>
         <Route path='/home' element={<Home/>}/> 
         <Route path="/profile" element={<Perfil/>}  /> 
+        <Route path='/about' element ={<About/> }/>
         <Route path= "*" element={<Error/>} />
       </Routes>        
     </div>
---
+++ src/view/About/About.jsx
@@ -0,0 +1,24 @@
+import './About.css'
+import { NavAbout } from '../../NavAbout/NavAbout'
+import { CardsAbout } from '../../CardsAbout/CardsAbout'
+export const About =()=>{
+    return(
+        <div className='abtCont'>
+            <NavAbout/>
+            
+            <div className='aboutDesc'>
+                <h1 className='aboutTitle'>Find your Teacher</h1>
+                <div className='aboutdes'>
+                    Find your teacher fue creado como una manera 
+                    <br></br>
+                    de ayudar a conectar a profesores de toda Latinoamerica
+                    <br></br>
+                     con  aquellos alumnos en busca de reforzar sus conocimientos en 
+                     <br></br>
+                     las materias de la escuela.
+                </div>
+            </div>
+            <CardsAbout/>
+        </div>
+    )
+}
