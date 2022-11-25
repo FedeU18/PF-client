784089f9eee80646fd8fd7e27362903e137083e8
fabian menjura <fabian.menjura.22@gmail.com>
2022-11-25 08:16:21 -0500

    agregue btn volver a home desde perfil alumno

--- src/view/PerfilProfesor/Detalle.jsx
+++ src/view/PerfilProfesor/Detalle.jsx
@@ -1,49 +1,56 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import "./Detalle.css";
-import img from "../../components/profesores/img/profecard.jpg"
-import { getProfesorById } from '../../redux/Actions/Profesor';
-
-
-
+// import img from "../../components/profesores/img/profecard.jpg"
+import { getProfesorById } from "../../redux/Actions/Profesor";
 
 export const Detalle = () => {
- 
- let {id} = useParams();
- let dispatch = useDispatch();
- let details = useSelector(state=>state.profesores.detail); 
-   console.log(details) 
- useEffect(()=>{
-  dispatch(getProfesorById(id))
- },[])
+  let { id } = useParams();
+  let dispatch = useDispatch();
+  let details = useSelector((state) => state.profesores.detail);
+  console.log(details);
+  useEffect(() => {
+    dispatch(getProfesorById(id));
+  }, []);
   return (
-      <>
-    
-    <div className='firstContainer' key={details.id}>
-           
-           <div className='detailContainer'>
-             
-             <img className="profImg"src= {details.image? details.image : img} alt="" width="380px"  height="300px"/>
-  
-              <div className="gameDetail">
-             
-                <h1>{details.nombre}</h1>
-              
-  
-              
-              
-              <div className='detailPlatform' ><strong>Email: </strong> {details.email } </div>
-              <p ><strong>materias:</strong>{details.materias?.map(e => e.name).join(", ")}</p>
-              <div><strong>Sobre mi: </strong>{<p dangerouslySetInnerHTML={{__html: details.descripcion}}></p>}</div>
-              <div  className="ratingDetail"> <strong> puntaje:</strong> <p className='ratingDetails'>{details.puntuacion}</p> </div>
-              </div>
-  
+    <>
+      <div className="firstContainer" key={details.id}>
+        <div className="detailContainer">
+          <img
+            className="profImg"
+            src={details.image ? details.image : img}
+            alt=""
+            width="380px"
+            height="300px"
+          />
+
+          <div className="gameDetail">
+            <h1>{details.nombre}</h1>
+
+            <div className="detailPlatform">
+              <strong>Email: </strong> {details.email}{" "}
+            </div>
+            <p>
+              <strong>materias:</strong>
+              {details.materias?.map((e) => e.name).join(", ")}
+            </p>
+            <div>
+              <strong>Sobre mi: </strong>
+              {
+                <p
+                  dangerouslySetInnerHTML={{ __html: details.descripcion }}
+                ></p>
+              }
+            </div>
+            <div className="ratingDetail">
+              {" "}
+              <strong> puntaje:</strong>{" "}
+              <p className="ratingDetails">{details.puntuacion}</p>{" "}
+            </div>
           </div>
-  
         </div>
-  
-        
-     </> 
-  )
-}
+      </div>
+    </>
+  );
+};
