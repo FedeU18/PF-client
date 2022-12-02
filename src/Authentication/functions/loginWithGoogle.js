47b8c5025ae19bf00a5b0c3b75fd163e8d6d3fdf
Michael Canales <CDLSMichael.16@gmail.com>
2022-12-02 12:02:17 -0500

    formularios con registro de google

--- src/Authentication/functions/loginWithGoogle.js
+++ src/Authentication/functions/loginWithGoogle.js
@@ -8,39 +8,80 @@ import setUserData from "./setUserData";
 import getCurrentUser from "./getCurrentUser";
 import { useDispatch } from "react-redux";
 import { postAlumno } from "../../redux/Actions/Alumno";
+import { postProfesor } from "../../redux/Actions/Profesor";
+import logOut from "./logOut";
+import deleteCurrentUser from "./deleteCurretUser";
 
 export default async function loginWithGoogle(form, dispatch) {
   try {
     const provider = new GoogleAuthProvider();
     await signInWithPopup(auth, provider);
-    console.log(provider);
 
     const currentAuthGoogle = auth.currentUser;
+    console.log(currentAuthGoogle);
 
-    if (form.rol === "student") {
-      dispatch(
-        postAlumno({
+    if (form) {
+      if (form.rol === "student") {
+        dispatch(
+          postAlumno({
+            id: currentAuthGoogle.uid,
+            email: currentAuthGoogle.email,
+            ...form,
+          })
+        );
+
+        await setUserData(currentAuthGoogle.uid, {
           id: currentAuthGoogle.uid,
           email: currentAuthGoogle.email,
           ...form,
-        })
-      );
+        });
 
-      await setUserData(currentAuthGoogle.uid, {
-        id: currentAuthGoogle.uid,
-        email: currentAuthGoogle.email,
-        ...form,
-      });
+        const dataFirestore = await getCurrentUser(currentAuthGoogle.uid);
 
+        const currentUserJSON = JSON.stringify(currentAuthGoogle);
+        const dataFirestoreJSON = JSON.stringify(dataFirestore);
+        localStorage.setItem("user", "true");
+        localStorage.setItem("userData", dataFirestoreJSON);
+        localStorage.setItem("userDataAuth", currentUserJSON);
+      } else if (form.rol === "teacher") {
+        dispatch(
+          postProfesor({
+            id: currentAuthGoogle.uid,
+            email: currentAuthGoogle.email,
+            ...form,
+          })
+        );
+
+        await setUserData(currentAuthGoogle.uid, {
+          id: currentAuthGoogle.uid,
+          email: currentAuthGoogle.email,
+          ...form,
+        });
+
+        const dataFirestore = await getCurrentUser(currentAuthGoogle.uid);
+
+        const currentUserJSON = JSON.stringify(currentAuthGoogle);
+        const dataFirestoreJSON = JSON.stringify(dataFirestore);
+        localStorage.setItem("user", "true");
+        localStorage.setItem("userData", dataFirestoreJSON);
+        localStorage.setItem("userDataAuth", currentUserJSON);
+      }
+    } else {
       const dataFirestore = await getCurrentUser(currentAuthGoogle.uid);
 
-      const currentUserJSON = JSON.stringify(currentAuthGoogle);
-      const dataFirestoreJSON = JSON.stringify(dataFirestore);
-      localStorage.setItem("user", "true");
-      localStorage.setItem("userData", dataFirestoreJSON);
-      localStorage.setItem("userDataAuth", currentUserJSON);
-    } else if (form.rol === "teacher") {
-      
+      if (!dataFirestore) {
+        await deleteCurrentUser();
+        await logOut(auth);
+        return "user no existe"
+      }
+      if (dataFirestore) {
+        const dataFirestoreJSON = JSON.stringify(dataFirestore);
+        const currentUserJSON = JSON.stringify(currentAuthGoogle);
+
+        localStorage.setItem("user", "true");
+        localStorage.setItem("userData", dataFirestoreJSON);
+        localStorage.setItem("userDataAuth", currentUserJSON);
+      }
     }
   } catch (error) {
     console.error(error);
--- src/view/Landing/Landing.jsx
+++ src/view/Landing/Landing.jsx
@@ -1,98 +1,165 @@
-import { useState } from 'react'
-import './Landing.css'
-import img from './img/google.png'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import logo from './img/1.png'
-import { Link } from 'react-router-dom'
-import LoginWithEmailPassword from '../../Authentication/functions/loginWithEmailAndPassword'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import "./Landing.css";
+import img from "./img/google.png";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import logo from "./img/1.png";
+import { Link } from "react-router-dom";
+import LoginWithEmailPassword from "../../Authentication/functions/loginWithEmailAndPassword";
+import { useNavigate } from "react-router-dom";
+import loginWithGoogle from "../../Authentication/functions/loginWithGoogle";
+import MessageError from "./MessageError";
 
 export const Landing = () => {
-    const [globalMessage, setGlobalMessage] = useState("")
-    const navigate = useNavigate()
+  const [messageError, setMessageError] = useState(false);
+  const [globalMessage, setGlobalMessage] = useState("");
+  const navigate = useNavigate();
 
-    const [input, setInput] = useState({
-        email: "",
-        password: ""
-    })
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleInput = (e) => {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        })
+  const loginGoogle = async (e) => {
+    e.preventDefault();
+    try {
+      const googleTryLoogin = await loginWithGoogle();
+      // console.log();
+      if (googleTryLoogin === "user no existe") {
+        setMessageError(true);
+        setTimeout(() => {
+          setMessageError(false);
+        }, 3000);
+        return;
+      }
+
+      navigate("/home");
+      // if(googleTryLoogin)
+      return;
+    } catch (error) {
+      console.error(error);
     }
+  };
 
+  const handleInput = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const register$ = await LoginWithEmailPassword(input.email, input.password)
-        if (typeof register$ === "string") {
-            setGlobalMessage(register$)
-            setTimeout(() => {
-                setGlobalMessage("")
-            }, 3000);
-        } else {
-            navigate('/home')
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const register$ = await LoginWithEmailPassword(input.email, input.password);
+    if (typeof register$ === "string") {
+      setGlobalMessage(register$);
+      setTimeout(() => {
+        setGlobalMessage("");
+      }, 3000);
+    } else {
+      navigate("/home");
     }
-    return (
-        <div className="fondo">
-            <div className="custom-shape-divider-bottom-1669219916">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill"></path>
-                    <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="shape-fill"></path>
-                    <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="shape-fill"></path>
-                </svg>
-            </div>
-            <div>
-                <img className='logo' src={logo} alt="logo" />
-                <Container className='container'>
-                    <Row className='two-col'>
-                        <Col className="formulario">
-                            <div id="contenedor">
-                                <div id="central">
-                                    <div id="login">
-                                        <div className="titulo">
-                                            Bienvenido
-                                        </div>
-                                        <form id="loginform">
-                                            <input onChange={(e) => handleInput(e)} type="text" name="email" placeholder="Email" value={input.email} />
+  };
+  return (
+    <div className="fondo position-relative">
+      {messageError && <MessageError />}
+      <div className="custom-shape-divider-bottom-1669219916">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+            opacity=".25"
+            className="shape-fill"
+          ></path>
+          <path
+            d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+            opacity=".5"
+            className="shape-fill"
+          ></path>
+          <path
+            d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+            className="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <img className="logo" src={logo} alt="logo" />
+        <Container className="container">
+          <Row className="two-col">
+            <Col className="formulario">
+              <div id="contenedor">
+                <div id="central">
+                  <div id="login">
+                    <div className="titulo">Bienvenido</div>
+                    <form id="loginform">
+                      <input
+                        onChange={(e) => handleInput(e)}
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        value={input.email}
+                      />
 
-                                            <input onChange={(e) => handleInput(e)} type="password" placeholder="Contraseña" name="password" value={input.password} />
+                      <input
+                        onChange={(e) => handleInput(e)}
+                        type="password"
+                        placeholder="Contraseña"
+                        name="password"
+                        value={input.password}
+                      />
 
-                                            <button type="submit" onClick={(e) => handleSubmit(e)} title="Ingresar" name="Ingresar">Login</button>
-                                        </form>
-                                        {globalMessage && <p className='text-danger text-center fw-bold mt-2'>{globalMessage}</p>}
-                                        <div className="pie-form">
-                                            <Link to="/forgot-password"
-                                                className='link'>¿Perdiste tu contraseña?</Link><br />
-                                            <Link to="/register" className='link'>¿No tienes Cuenta? Registrate</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </Col>
-                        <Col className='text'>
-                            <h1 className='title'>Encuentra un profesor particular</h1>
-                            <div>
-                                Find your teacher fue creado como una manera 
-                                <br></br>
-                                de ayudar a conectar a profesores de toda Latinoamerica
-                                <br></br>
-                                con  aquellos alumnos en busca de reforzar sus conocimientos en 
-                                <br></br>
-                                las materias de la escuela.
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    )
-}
-
+                      <button
+                        type="submit"
+                        onClick={(e) => handleSubmit(e)}
+                        title="Ingresar"
+                        name="Ingresar"
+                      >
+                        Login
+                      </button>
+                    </form>
+                    {globalMessage && (
+                      <p className="text-danger text-center fw-bold mt-2">
+                        {globalMessage}
+                      </p>
+                    )}
+                    <div className="pie-form">
+                      <Link to="/forgot-password" className="link">
+                        ¿Perdiste tu contraseña?
+                      </Link>
+                      <br />
+                      <Link to="/register" className="link">
+                        ¿No tienes Cuenta? Registrate
+                      </Link>
+                    </div>
+                    <div>
+                      <button type="button" onClick={loginGoogle}>
+                        google
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col className="text">
+              <h1 className="title">Encuentra un profesor particular</h1>
+              <div>
+                Find your teacher fue creado como una manera
+                <br></br>
+                de ayudar a conectar a profesores de toda Latinoamerica
+                <br></br>
+                con aquellos alumnos en busca de reforzar sus conocimientos en
+                <br></br>
+                las materias de la escuela.
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
