fd1e9a3f451ab29991e497c71d60550fe16081b7
Gianella Leon <giigl@hotmail.com>
2022-12-03 20:54:08 -0500

    añadi formularios

--- src/redux/Reducer/index.js
+++ src/redux/Reducer/index.js
@@ -4,12 +4,14 @@ import materiasReducer from "./materiasReducer";
 import paisesReducer from "./paisesReducer";
 import alumnosReducer from "./alumnosReducer";
 import comentariosReducer from "./comentariosReducer";
+import certificadosReducer from "./certificadosReducer"
 const rootReducer = combineReducers({
   profesores: profesoresReducer,
   materias: materiasReducer,
   paises:paisesReducer,
   alumnos: alumnosReducer,
   comentarios:comentariosReducer,
+  certificados:certificadosReducer,
 });
 
 export default rootReducer;
