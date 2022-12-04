69952bfa81e19cec7a631aa92841e3df6f6b935f
Federico Uñates <fedee.unates.2001@gmail.com>
2022-12-04 16:31:21 -0300

    Calendario casi terminado

--- src/redux/Reducer/index.js
+++ src/redux/Reducer/index.js
@@ -4,12 +4,15 @@ import materiasReducer from "./materiasReducer";
 import paisesReducer from "./paisesReducer";
 import alumnosReducer from "./alumnosReducer";
 import comentariosReducer from "./comentariosReducer";
+import fechasReducer from "./fechaReducer";
+
 const rootReducer = combineReducers({
   profesores: profesoresReducer,
   materias: materiasReducer,
   paises:paisesReducer,
   alumnos: alumnosReducer,
   comentarios:comentariosReducer,
+  fechas: fechasReducer
 });
 
 export default rootReducer;
