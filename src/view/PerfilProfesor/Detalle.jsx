879a4cc47dfb857284e14a91cfa2b91bda6281be
fabian menjura <fabian.menjura.22@gmail.com>
2022-12-05 01:46:01 -0500

    merg

--- src/view/PerfilProfesor/Detalle.jsx
+++ src/view/PerfilProfesor/Detalle.jsx
@@ -13,31 +13,25 @@ import { Certificados } from "../../components/Certificados/Certificados";
 import { Reseñas } from "../../components/Reseñas/Reseñas";
 import { Comentarios } from "../../components/Comentarios/Comentarios";
 import { AddComent } from "../../components/AddComent/AddComent";
-import userAuthenticate from "../../Authentication/functions/user";
+
 import { GrAdd } from "react-icons/gr";
 import userAuthentication from "../../Authentication/functions/user";
 import * as actionsAlumno from "../../redux/Actions/Alumno";
-
-import { ChatProfesor } from "../../components/Chat/chatProfesor";
-import { ChatAlumno } from "../../components/Chat/chatAlumno";
-
 import Calendario from "../../components/Calendario/Calendario";
-
-
-
+import { ChatProfesor } from "../../components/chat/chatProfesor";
+import { ChatAlumno } from "../../components/chat/chatAlumno";
 
 export const Detalle = () => {
   let { id } = useParams();
   let dispatch = useDispatch();
   let details = useSelector((state) => state.profesores.detail);
   let infoAlumno = useSelector((state) => state.alumnos.alumno);
-  console.log("doy detalles de profesor", details);
 
   const [current, setCurrent] = useState("Información");
   const [openFotos, setOpenFotos] = useState(false);
   const [show, setShow] = useState(false);
   const { userData } = userAuthentication();
-  console.log("soy user data desde detalles ", userData);
+  console.log(infoAlumno);
 
   useEffect(() => {
     dispatch(actionsAlumno.getAlumnoFromAPI(userData.id));
@@ -61,7 +55,6 @@ export const Detalle = () => {
   const handleCloseFotos = () => {
     setOpenFotos(false);
   };
-
   return (
     <div className="detalleProfeContainer">
       <Certificados
@@ -168,53 +161,27 @@ export const Detalle = () => {
                 </div>
               </div>
 
-
-                  <div className="certiMateCont">
-                    <div className="certiContDe">
-                      <span className="subTitleDe" >Estudios y Certificados:</span>
-                      {details.certificados?.length===0 &&(
-                          <div className="certiNoCont">
-                            <span className="">
-                            Este author aun no tiene certificados que mostrar.
-                            </span>
-                          </div>)}
-
-                      {details.certificados?.length>0 && details.certificados?.length<7 &&(
-                          <div className="certiSiCont">
-                            {details.certificados.map(c=>(
-                            <div key={c} className='fotoCertificadoDeco' onClick={handleOpenFotos}>
-                              <img src={c.foto} className='fotoCertificadoDe'/>
-                            </div>))}
-                          </div>)}
-                      {details.certificados?.length>6 && (
-                          <div className="certiSiCont">
-                            {details.certificados.map((c,i)=>(
-                                <>
-                                  {i<5 && (
-                                    <div key={i} className='fotoCertificadoDeco'
-                                    onClick={handleOpenFotos}>
-                                    <img src={c.foto} className='fotoCertificadoDe'/>
-                                    </div>
-                                  )}
-                                </>
-                            ))}
-                             <div className='fotoCertificadoDeco fcdcPlus' 
-                                  onClick={handleOpenFotos}>
-                                    +{details.certificados.length-5}
-                              </div>
-                          </div>)}
-                      
+              <div className="certiMateCont">
+                <div className="certiContDe">
+                  <span className="subTitleDe">Estudios y Certificados:</span>
+                  {details.certificados?.length === 0 && (
+                    <div className="certiNoCont">
+                      <span className="">
+                        Este author aun no tiene certificados que mostrar.
+                      </span>
                     </div>
-                    
-                    <div className="materiasContDe">
-                      <span className="subTitleDe">Materias:</span>
-                      {details.materias?.length>0 && details.materias.map(m=>(
-                        <div key={m} className="nameLogoMaDeCont">
-                          <div className="logoMaDeContDo">
-                            <div className="logoMaDeCont">
-                            <img src={`/${m.name}.png`} className='logoMaDe'/>
-                          </div>
+                  )}
 
+                  {details.certificados?.length > 0 &&
+                    details.certificados?.length < 7 && (
+                      <div className="certiSiCont">
+                        {details.certificados.map((c) => (
+                          <div
+                            key={c}
+                            className="fotoCertificadoDeco"
+                            onClick={handleOpenFotos}
+                          >
+                            <img src={c.foto} className="fotoCertificadoDe" />
                           </div>
                         ))}
                       </div>
@@ -225,6 +192,7 @@ export const Detalle = () => {
                         <>
                           {i < 5 && (
                             <div
+                              key={i}
                               className="fotoCertificadoDeco"
                               onClick={handleOpenFotos}
                             >
@@ -247,61 +215,23 @@ export const Detalle = () => {
                   <span className="subTitleDe">Materias:</span>
                   {details.materias?.length > 0 &&
                     details.materias.map((m) => (
-                      <div className="nameLogoMaDeCont">
+                      <div key={m} className="nameLogoMaDeCont">
                         <div className="logoMaDeContDo">
                           <div className="logoMaDeCont">
                             <img src={`/${m.name}.png`} className="logoMaDe" />
                           </div>
                         </div>
-
                         <div className="nameMaDeCont">{m.name}</div>
                       </div>
                     ))}
                 </div>
               </div>
-
-                      )
-                      )}
-                    </div>
-                  </div>
-                  
-                </div>)}
-            {current==='Calendario' && (
-                <div className="subContDe">
-                  <Calendario profe={details} />
-                </div>)}
-            {current==='Reseña' && (
-                <div className="subContDe">
-                  
-                  <Reseñas puntajes={details.puntuacions}/>
-                  <br></br>
-                  <div className="comentsBtnAddCont">
-                    <Comentarios myId={userData.id}
-                                profileOwner={details.id} 
-                                coments={details.coments} />
-                    {userData.id!== details.id && infoAlumno.tipo &&(
-                      <button className="btnAddComOp" onClick={handleShow}>
-                      +
-                    </button>
-                    )}
-                  </div>
-                  
-                  <AddComent  alumnoId={infoAlumno.id} 
-                              myId={userData.id}
-                              profesorId={details.id}
-                               show={show} 
-                               handleClose={handleClose}/>
-
-                </div>)}
-            {current==='Chat' && (
-                <div className="subContDe">
-                  Chat
-                </div>)}
-
             </div>
           )}
           {current === "Calendario" && (
-            <div className="subContDe">Calendario</div>
+            <div className="subContDe">
+              <Calendario profe={details} />
+            </div>
           )}
           {current === "Reseña" && (
             <div className="subContDe">
