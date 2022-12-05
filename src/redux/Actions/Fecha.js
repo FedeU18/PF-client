14e732071847d7cd5116088eb615a2d05a3529e5
Michael Canales <CDLSMichael.16@gmail.com>
2022-12-05 01:35:11 -0500

    añadendo pasarella

--- src/redux/Actions/Fecha.js
+++ src/redux/Actions/Fecha.js
@@ -2,25 +2,25 @@ import axios from "axios";
 import { GET_FECHA, POST_FECHA } from "../types/typesFecha";
 
 export const getFecha = () => {
-    return async function (dispatch){
-        const fechas = await axios.get('/fechas')
-        return dispatch({
-            type: GET_FECHA,
-            payload: fechas.data
-        })
-    }
-}
+  return async function (dispatch) {
+    const fechas = await axios.get("/fechas");
+    return dispatch({
+      type: GET_FECHA,
+      payload: fechas.data,
+    });
+  };
+};
 
 export const postFecha = (payload) => {
-    return async function(){
-        try {
-            const post = axios.post('/fechas',payload)
-            return dispatch({
-                type: POST_FECHA,
-                payload: post.data
-            })
-        } catch (error) {
-            console.log("hubo un error al reservar una fecha", error)
-        }
+  return async function (dispatch) {
+    try {
+      const post = axios.post("/fechas", payload);
+      return dispatch({
+        type: POST_FECHA,
+        payload: post.data,
+      });
+    } catch (error) {
+      console.log("hubo un error al reservar una fecha", error);
     }
-}
+  };
+};
