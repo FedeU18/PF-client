f8f5acb0b913b73ed7b6cf653416fb556f555c2c
gastonf-git <flora.gaston@outlook.com>
2022-12-15 01:54:43 -0300

    price order and country filter

--- src/view/Home/NavFiltros.jsx
+++ src/view/Home/NavFiltros.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import "./NavFiltros.css";
+import { sortPriceBack } from "../../redux/Actions/Profesor";
+import { filterProfeCountryBack } from "../../redux/Actions/Profesor";
 
 //dispachar la accion de traer paises.
 
 function NavFiltros() {
   const paises = useSelector((state) => state.paises.paises);
+  const dispatch = useDispatch();
+
+
+  function onSelectSortChange(e) {
+    e.preventDefault();
+   
+   dispatch(sortPriceBack(e.target.value));
+    
+    
+
+  }
+
+  function onSelectFilter(e) {
+    e.preventDefault();
+   
+   dispatch(filterProfeCountryBack(e.target.value));
+    
+    
+
+  }
+
+
 
   return (
     <div>
@@ -13,17 +37,17 @@ function NavFiltros() {
       {/*-----------FILTROS--------------------*/}
       <div className="fab-contenedor-selectores">
         <div className="fab-caja-filtros">
-          <select className="fab-select">
-            <option>Precio</option>
-            <option value="asc">Costoso</option>
-            <option value="des">Barato</option>
+          <select className="fab-select" onChange={onSelectSortChange}>
+            <option>Precio</option> 
+            <option value="asc">Menor</option>
+            <option value="desc">Mayor</option>
           </select>
           <select className="fab-select">
             <option>Puntuacion</option>
             <option value="asc">Mayor</option>
-            <option value="des">Menor</option>
+            <option value="desc">Menor</option>
           </select>
-          <select className="fab-select">
+          <select className="fab-select" onChange={onSelectFilter}>
             <option>Paises</option>
             {paises.length > 0 &&
               paises.map((el) => <option key={el.name}>{el.name}</option>)}
