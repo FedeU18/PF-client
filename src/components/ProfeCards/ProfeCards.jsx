33b052ab3c85c000eca639d07d43cabf9e6f0ec6
Gianella Leon <giigl@hotmail.com>
2022-12-14 23:39:10 -0500

    prof card error

--- src/components/ProfeCards/ProfeCards.jsx
+++ src/components/ProfeCards/ProfeCards.jsx
@@ -2,14 +2,17 @@ import { ProfeCard } from "../ProfeCard/Profecard";
 import "./ProfeCards.css";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 export const ProfeCards = ({ profes, msgUsuariosAlumno, socket }) => {
+  const repeat =[1,2,3,4,5,6,7,8]
   return (
     <div className="ProfeCardsCont">
       {profes && profes.length > 0 ? (
         profes?.map((e, index) => {
           let active = msgUsuariosAlumno.includes(e.nombre);
-          return e.Error ? (
+          return e.error ? (
             <h4 key={index}>profesor no encontrado</h4>
           ) : (
             <div className="homeProfeCard" key={index}>
@@ -30,8 +33,23 @@ export const ProfeCards = ({ profes, msgUsuariosAlumno, socket }) => {
           );
         })
       ) : (
-        <div>
-          <h1 className="3">Cargando...</h1>
+        <div className="ProfeCardsCont">
+          
+          {repeat.map((r)=>(
+            <Card style={{ width: '14rem'  ,heigth:'20rem'}}>
+            <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png" />
+            
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={8} />
+              
+            </Placeholder>
+              
+            
+          </Card>
+          ))}
+        
+        
+          
         </div>
       )}
     </div>
